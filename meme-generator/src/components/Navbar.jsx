d979ec06a83e2393078b72ba5f9f875571df2020
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2021-12-06 12:22:42 +0530

    Meme generator React setup

---
+++ meme-generator/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                   
+                        {/* This is Navbar. */}
+                        {/* Create the Navabar Here  */}
+                   
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Navbar
